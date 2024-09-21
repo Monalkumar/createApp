6555709276a681ac34842de283c15a93d50e1f9c
MonalKumar <monalshiv123@gmail.com>
2024-09-21 16:44:12 +0530

    multiple router style

--- src/App.js
+++ src/App.js
@@ -2,20 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.get("/users", (req, res) => {
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
   res.send({ name: "monal kumar", email: "monalshiv123@gmail.com" });
-});
-
-app.post("/employees", (req, res) => {
-  res.send("post apis successfully");
-});
-
-app.delete("/dlt", (req, res) => {
-  res.send("delet negative thoughts");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("hello programmer");
 });
 
 app.listen(1111, () => {
